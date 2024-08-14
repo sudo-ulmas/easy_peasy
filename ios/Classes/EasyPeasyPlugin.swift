0a815bfd7820cdea97711ac35ffab608e8cb8aa1
Abdulloh Raxmatullaev <a.raxmatullaev@yagona.io>
2024-08-14 14:43:22 +0500

    add delegate

--- ios/Classes/EasyPeasyPlugin.swift
+++ ios/Classes/EasyPeasyPlugin.swift
@@ -2,18 +2,32 @@ import Flutter
 import UIKit
 import MyIDNative
 
-public class EasyPeasyPlugin: NSObject, FlutterPlugin {
+public class EasyPeasyPlugin: NSObject, FlutterPlugin, HelloDelegate {
+    public func onSuccess(code: String) {
+        <#code#>
+    }
+    
+    
+    public func onError(exception: String) {
+        <#code#>
+    }
+    
+    public func onUserExited() {
+        <#code#>
+    }
+    
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "easy_peasy", binaryMessenger: registrar.messenger())
     let instance = EasyPeasyPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
+  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult)  {
+    switch call.method  {
     case "getPlatformVersion":
-        MyIDNative.Hello.startMyIdjon()
-      result("iOS " + UIDevice.current.systemVersion)
+        let hello = Hello()
+        hello.delegate = self
+        hello.startMyIdjon()
     default:
       result(FlutterMethodNotImplemented)
     }
